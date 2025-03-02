da43fb752550d1e925280720189b6d0d86fcd812
andateLP <paraquesevayaluis@gmail.com>
2025-03-02 18:26:54 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,15 +16,15 @@ client = tweepy.Client(
     access_token_secret=access_token_secret
 )
 
-# Fin del mandato de Luis Lacalle Pou
-finish_date = datetime(2025, 3, 1)
+# Fin del mandato de Yamandú Orsi
+finish_date = datetime(2030, 3, 1)
 
 # Obtener fecha actual
 today = datetime.now()
 today = today.replace(hour=0, minute=0, second=0, microsecond=0)
 
 # Obtener inicio del mandato
-start_date = datetime(2020, 3, 1)
+start_date = datetime(2025, 3, 1)
 
 # Función para calcular los días restantes
 def days_until():
@@ -38,13 +38,13 @@ def days_since():
 
 # Función para calcular el porcentaje
 def percentage_left(days_past):
-    total = datetime(2025, 3, 1) - datetime(2020, 3, 1)
+    total = datetime(2030, 3, 1) - datetime(2025, 3, 1)
     return round(((days_past * 100) / total.days),2)
 
 days_left = days_until()
 days_past = days_since()
 left = percentage_left(days_past)
-tweet_text = f"Falta{'n' if (days_left != 1) else ''} {days_left} días para que se vaya Luis y asuma @OrsiYamandu. Ya pasó el {left}% de mandato ({days_past} días)."
+tweet_text = f"Van {days_past} días de mandato de @OrsiYamandu. Ya pasó el {left}% de mandato."
 
 # Función para enviar el tweet
 def send_tweet(tweet_text):
